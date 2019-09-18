10b2ff42f951ce75a4dd89316ba8b93ed6fc8785
SteveScott <stevescott517@gmail.com>
2019-09-18 14:41:21 -0400

    Add files via upload

---
+++ boston_analysis.py
@@ -0,0 +1,24 @@
+from sklearn import datasets
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+boston = datasets.load_boston()
+df = pd.DataFrame(boston.data, columns=boston.feature_names)
+df['target'] = boston['target']
+instance_count, attr_count = df.shape
+pearson = df.corr(method='pearson')
+corr_with_target = pearson.iloc[-1][:-1]
+print(corr_with_target)
+corr = df.corr()
+sns.set_palette("BrBG",7)
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(corr,
+            cmap=("BrBG"),
+            square=True,
+            annot=True,
+            linewidths=1,
+            ax=ax)
+sns.pairplot(df)
+sns.jointplot(df["NOX"], df["INDUS"])
+
